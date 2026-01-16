e3b4ca32c1d19af0aa0bd88a0c42103fdf2ed0c2
Hannah Dadd <hannahbdadd@googlemail.com>
2026-01-16 09:47:49 +0000

    strrched button

--- Writing Games/Games/SplitWord/SplitWordGame.swift
+++ Writing Games/Games/SplitWord/SplitWordGame.swift
@@ -5,3 +5,21 @@
 //  Created by Hannah Dadd on 16/01/2026.
 //
 
+import SwiftUI
+
+struct SplitWordGame: View {
+    @State var word = GamesGlobalVariables.vocabMap.shuffled().first
+    var action: () -> Void
+    
+    var body: some View {
+        VStack {
+            VocabQuestion(word: word?.key ?? "", definition: word?.value ?? "", options: fakeWords, nextCard: {
+                word = GamesGlobalVariables.vocabMap.shuffled().first
+                fakeWords = GamesGlobalVariables.vocabMap.shuffled().prefix(3).compactMap { $0.value }
+            })
+            Spacer()
+            StretchedButton(text: "Done", action: action)
+        }
+        .padding()
+    }
+}
