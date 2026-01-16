58369e85831f22fd3edcca9816e19d4bb89b140b
Hannah Dadd <hannahbdadd@googlemail.com>
2026-01-16 16:37:30 +0000

    grammar game

---
+++ Writing Games/Games/Editing/GrammarGame.swift
@@ -0,0 +1,45 @@
+//
+//  GrammarGame.swift
+//  Writing Games
+//
+//  Created by Hannah Dadd on 16/01/2026.
+//
+
+import SwiftUI
+
+struct GrammarGame: View {
+    @State private var showError = false
+    @State private var showCorrect = false
+    @State var response: String = ""
+    let sentence = GamesGlobalVariables.grammarSentences.shuffled().first!
+    var back: () -> Void
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 24) {
+            Text("Fix the grammar in this sentence:")
+            Text(sentence.0).bold()
+            TextEditor(text: $response)
+                .frame(height: 100, alignment: .leading)
+                .cornerRadius(6.0)
+                .border(Color.gray, width: 1)
+                .multilineTextAlignment(.leading)
+            Spacer()
+            StretchedButton(text: "Done", action: {
+                if sentence.1 == response {
+                    showCorrect = true
+                } else {
+                    showError = false
+                }
+            })
+        }
+        .padding()
+        .alert("Not quite! Try again", isPresented: $showError) {
+            Button("Close", role: .cancel) { }
+        }
+        .alert("You got it!", isPresented: $showCorrect) {
+            Button("Close", role: .cancel) {
+                
+            }
+        }
+    }
+}
