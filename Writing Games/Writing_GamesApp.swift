dca95bb8f6d211da20da182d88b34fd11f28bdcf
Hannah Dadd <hannahbdadd@googlemail.com>
2026-01-16 09:38:20 +0000

    Initial Commit

---
+++ Writing Games/Writing_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Writing_GamesApp.swift
+//  Writing Games
+//
+//  Created by Hannah Dadd on 16/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Writing_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
