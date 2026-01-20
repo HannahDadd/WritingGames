4e532aff5ebb4262db464ee4869e8b62e07d4f4f
Hannah Dadd <hannahbdadd@googlemail.com>
2026-01-20 18:03:34 +0000

    LivesAndScore

---
+++ Writing Games/Games/SharedComponents/LivesAndScore.swift
@@ -0,0 +1,25 @@
+//
+//  VocabSpace.swift
+//  Writing Games
+//
+//  Created by Hannah Dadd on 20/01/2026.
+//
+
+import SwiftUI
+
+struct LivesAndScore: View {
+    @Binding var hearts: Int
+    @Binding var total: Int
+    
+    var body: some View {
+        HStack {
+            Text("Score: **\(total)**")
+            Spacer()
+            if hearts > 0 {
+                ForEach(0..<hearts, id: \.self) { _ in
+                    Image(systemName: "heart.fill")
+                }
+            }
+        }
+    }
+}
--- Writing Games/Games/TimedGames/Vocab/VocabGame.swift
+++ Writing Games/Games/TimedGames/Vocab/VocabGame.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct VocabGame: View {
     @State var playing = false
+    @State var showEnd = false
     @State var hearts = 3
     @State var total = 0
 
@@ -18,17 +19,11 @@ struct VocabGame: View {
     var body: some View {
         if !playing {
             LoadingPage(title: GameTypes.vocabGame.getTitle(), subtitle: "Pick the correct meaning of these niche words to improve your vocabulary.", colour: Color.timedGames, icon: GameTypes.vocabGame.getIcon(), action: { playing = true })
-        } else if hearts == 0 {
+        } else if showEnd {
             EndPage(title: GameTypes.vocabGame.getTitle(), subtitle: "You're out of hearts!", colour: Color.timedGames, icon: GameTypes.vocabGame.getIcon(), action: { playing = true })
         } else {
             VStack(spacing: 8) {
-                HStack {
-                    Text("Score: **\(total)**")
-                    Spacer()
-                    ForEach(0..<hearts, id: \.self) { _ in
-                        Image(systemName: "heart.fill")
-                    }
-                }
+                LivesAndScore(hearts: $hearts, total: $total)
                 Spacer()
                 VocabQuestion(word: word?.key ?? "", definition: word?.value ?? "", options: fakeWords, onCorrect: {
                     total = total + 1
